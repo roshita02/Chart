1209c13e77d3959069c713c911aaeca8131ff2f1
roshita02 <roshitashakya07@gmail.com>
2019-12-21 13:37:18 +0545

    controller and route setup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  controller :home do
+    get :convert
+    get :pdf
+  end
 end
