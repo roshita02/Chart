e464ec84fa46dd8a2333dbd1ea708feff13d8236
roshita02 <roshitashakya07@gmail.com>
2019-12-21 13:45:02 +0545

    convert method added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def pdf
+  end
+
+  def convert
+  	_url = request.base_url + pdf_path
+  	_pdf = Dhalang::PDF.get_from_url(_url)
+  	_file_name = "Report" 
+    File.open("#{Rails.root}/public/#{_file_name}.pdf", "w+b") << _pdf
+	  redirect_to "/#{_file_name}.pdf"
+  end
 end
